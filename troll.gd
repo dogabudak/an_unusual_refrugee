c57c2691e5989ec328421fe2d56981486c7108d8
Doga Budak <dogabudak@gmail.com>
2023-04-15 13:37:56 +0200

    Changed movement to work only with mouse click

--- troll.gd
+++ troll.gd
@@ -1,13 +1,14 @@
 extends KinematicBody2D
 
-const MOTION_SPEED = 160 # Pixels/second.
-const TAN30DEG = tan(deg2rad(30))
+# Define the size of the grid and the size of each cell
+const GRID_SIZE = Vector2(10, 10)
+const CELL_SIZE = Vector2(32, 32)
 
-func _physics_process(_delta):
-	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	motion.y *= TAN30DEG
-	motion = motion.normalized() * MOTION_SPEED
-	#warning-ignore:return_value_discarded
-	move_and_slide(motion)
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			var click_pos = get_global_mouse_position()
+			var cell_x = int(click_pos.x / CELL_SIZE.x)
+			var cell_y = int(click_pos.y / CELL_SIZE.y)
+			var world_pos = Vector2(cell_x, cell_y) * CELL_SIZE
+			position = world_pos + CELL_SIZE / 2.0
